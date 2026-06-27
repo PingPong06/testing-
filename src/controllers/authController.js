6d26a61ead20b93c7f1b77dfe9c15f0eec47f791
Darshan_Wagh <darshanwaghwce@gmail.com>
2026-06-27 11:13:41 +0530

    May be Final Testing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -141,6 +141,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
 });
 // console.log("EMAIL_USER:", process.env.EMAIL_USER);
 // console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
