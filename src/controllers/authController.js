d4fa34ff84822ddb20e80f4d67a9afc6cbc03c56
Darshan_Wagh <darshanwaghwce@gmail.com>
2026-06-27 10:51:56 +0530

    Use Gmail service for Nodemailer

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -135,13 +135,12 @@ const forgotPassword = async (req, res) => {
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  service: "gmail",
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 // console.log("EMAIL_USER:", process.env.EMAIL_USER);
 // console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
