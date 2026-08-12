81cd815fd5523adb894d28625c265cdffd84e473
Darshan_Wagh <darshanwaghwce@gmail.com>
2026-08-12 18:00:41 +0530

    still updating db

--- src/config/db.js
+++ src/config/db.js
@@ -2,36 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
-
-// pool.query("SELECT current_database(), current_user")
-//   .then(result => {
-//     console.log("Database:", result.rows[0]);
-//   })
-//   .catch(err => {
-//     console.error("Database connection check failed:", err);
-//   });
-
-const pool = new Pool({
-  
   connectionString: process.env.DATABASE_URL,
-
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? {
-          rejectUnauthorized: false,
-        }
-      : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-console.log(
-  "Connected to DB:",
-  process.env.DATABASE_URL
-);
 
 module.exports = pool;
